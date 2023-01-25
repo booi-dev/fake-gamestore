36128464c4b6d7c32b6fe98cfd1c64a0d24565f4
Booi <56810550+booimangang@users.noreply.github.com>
2023-01-25 16:08:32 +0530

    Add basic layouts

---
+++ src/layouts/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function footer() {
+    return (
+        <div>footer</div>
+    );
+}
+
+export default footer;
---
+++ src/layouts/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function main() {
+  return (
+    <div>main</div>
+  );
+}
+
+export default main;

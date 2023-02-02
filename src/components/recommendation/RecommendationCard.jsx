92171594e7d8cb0785e76e9790533b8e69e63897
Booi <56810550+booimangang@users.noreply.github.com>
2023-02-03 00:38:40 +0530

    Add responsiveness to recommendation card

--- src/components/recommendation/RecommendationCard.jsx
+++ src/components/recommendation/RecommendationCard.jsx
@@ -3,12 +3,26 @@ import './RecommendationCard.scss';
 
 function RecommendationCard(props) {
     const { game } = props;
+
     return (
         <div className="recommendation-card" >
-            <img src={game.background_image} alt={`${game.name} bg img`}
+            <img src={game?.background_image} alt={`${game.name} bg img`}
                 className="recommendation-card_bg-img"
             />
-            <h2>{game.name} </h2>
+            <div className="recommendation-card_game-details">
+                <h2 className="rg-game-title">{game?.name} </h2>
+                <div className="rg-sreenshots-container">
+                    {game?.short_screenshots.slice(0, 4).map(sss => <img src={sss.image} alt={`${game.name} screenshot`} key={sss.id} className="rg-screenshots" />)}
+                </div>
+                <div>
+                    {game?.genres.slice(0, 4).map(genre => <span key={genre.id}
+                        className="rg-genres"
+                    >{genre.name}</span>)}
+                </div>
+
+                <h3 className="rg-released-date">released on: {game?.released} </h3>
+            </div>
+
         </div>
     );
 }

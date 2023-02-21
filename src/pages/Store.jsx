0ffa3de801bea3a9d22e55babb6c5c573dae4ee1
Booi <56810550+booimangang@users.noreply.github.com>
2023-02-22 00:57:51 +0530

    Remove browse category from store page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,6 @@
 import { Element } from "react-scroll";
 
 import StoreHero from "../components/store/StoreHero";
-import BrowseCategory from "../components/categories/BrowseCategory";
 import GameLatest from "../components/game/GameLatest";
 import Footer from '../layouts/Footer';
 
@@ -12,10 +11,9 @@ function Store() {
             <StoreHero />
             <Element name="browse-games-title" />
             <div className="app-flex-center" id="browse-games-title">
-                <h1 className="app-group-title fs-xl"> BROWSE LATEST GAMES</h1>
+                <h1 className="app-group-title fs-xl"> LATEST GAMES</h1>
             </div>
             <GameLatest />
-            <BrowseCategory />
             <Footer />
         </div>
     );

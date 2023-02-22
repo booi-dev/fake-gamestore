27de6c777bac8f605b78550ad50ac9f842c27df2
Booi <56810550+booimangang@users.noreply.github.com>
2023-02-22 10:22:20 +0530

    Change from BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AccountProvider } from './context/useAccount';
 import { WishCartProvider } from './context/useWishCart';
-// import { HeaderRefProvider } from './layouts/Header';
 import App from './App';
 import './index.scss';
 
@@ -12,11 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <AccountProvider>
         <WishCartProvider>
-          {/* <HeaderRefProvider> */}
           <App />
-          {/* </HeaderRefProvider> */}
         </WishCartProvider>
       </AccountProvider>
     </Router>
   </React.StrictMode>,
 );
+
+// main routing is in MainBody component. 

8f2d66c8ee8ecf638de07c8620fee286b6d54aaf
Booi <56810550+booimangang@users.noreply.github.com>
2023-02-22 23:10:17 +0530

    Add func to fetch latest game from the last year

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 custom date, to find out latest
 revisit search
-
-hide scroller scroll-bar - mobile site
-
-show hover effect on - mobile site
--- src/components/game/GameLatest.jsx
+++ src/components/game/GameLatest.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from 'react';
+import { subYears, format } from 'date-fns';
 import fetchData, { createQuery, createMultiQuery } from '../../utils/fetch';
 import GameGallery from './GameGallery';
+
+const currentDate = new Date();
+const formatedCurrentDate = format(currentDate, 'yyyy-MM-dd');
+const dateOneYearAgo = subYears(currentDate, 1);
+const formatedDateOneYearAgo = format(dateOneYearAgo, 'yyyy-MM-dd');
+
+const latestYearRange = `${formatedDateOneYearAgo},${formatedCurrentDate}`;
 
 function GameBrowse() {
 
     const [gameData, setGameData] = useState();
 
     const fetchGameData = async function fetchGameData() {
-        const newDatesQuery = createQuery('dates', '2022-01-01,2023-02-16');
-        const newOrderingQuery = createQuery('ordring', '-released');
+        const newDatesQuery = createQuery('dates', latestYearRange);
+        const newOrderingQuery = createQuery('ordering', '-rating');
         const newPageSizeQuery = createQuery('page_size', 12);
 
         const query = createMultiQuery([newDatesQuery, newOrderingQuery, newPageSizeQuery]);
